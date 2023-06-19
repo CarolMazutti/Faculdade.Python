5fc829a04da81149f611b1b3c25a1378a342fce2
Carol Mazutti <127802847+CarolMazutti@users.noreply.github.com>
2023-06-18 22:46:25 -0300

    Resolução incompleta, atv 01.03

---
+++ Segundo Bimestre/Atividade01.03.py
@@ -0,0 +1,13 @@
+#Fazer um programa que peça dois valores (X e Y) no programa principal e uma função realize a troca dos valores, ou seja, X recebe o conteúdo de Y e Y receberá o conteúdo de X.
+
+x=int(input('Digite o primeiro valor: '))
+y=int(input('Digite o segundo valor: '))
+
+def trocaNum(x, y):
+    
+    return x
+
+
+troca1 = trocaNum
+troca2 = trocaNum2
+print(f'O valor de x e y é: {troca1} {troca2}')
